2a40b61e69438f044d2e369bbc1758c9175fc862
kumawatjitu <89432379+kumawatjitu@users.noreply.github.com>
2022-07-28 08:33:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -28,4 +28,5 @@ def predict():
     return render_template('index.html', prediction_text='Regression Model  has predicted price for given experinace is : {}'.format(prediction))
 
 
-app.run()
+if __name__=='__main__':
+  app.run(debug=True)
